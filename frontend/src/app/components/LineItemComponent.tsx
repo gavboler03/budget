5c4b5bd50a16a60d828324a0f400d66617177eea
gavboler03 <89207920+gavboler03@users.noreply.github.com>
2026-01-10 19:41:11 -0600

    line item updates

--- frontend/src/app/components/LineItemComponent.tsx
+++ frontend/src/app/components/LineItemComponent.tsx
@@ -3,21 +3,50 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { LineItem } from "../objects/LineItemObject";
+import { Budget } from "../objects/BudgetObject";
 
 interface Props {
+  budgetObject: Budget;
   lineItemObject: LineItem;
 }
 
-export default function LineItemComponent({ lineItemObject }: Props) {
+export default function LineItemComponent({
+  budgetObject,
+  lineItemObject,
+}: Props) {
   const [lineItem, setLineItem] = useState<LineItem | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [editing, setEditing] = useState(false);
+
+  const { id } = budgetObject;
+  const { item_id, title, amount } = lineItemObject;
 
   const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000",
   });
 
   useEffect(() => {
-    api.get(``);
+    api
+      .get(`/budgets/budget/${id}/line-items/${item_id}`)
+      .then((response) => {
+        setLineItem(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching items:", error);
+        setError("Cannot fetch line items");
+      })
+      .finally(() => setLoading(false));
   });
+
+  if (loading) {
+    return <p>Loading budgets...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return <div>LineItemComponent</div>;
 }
--- frontend/src/app/objects/LineItemObject.tsx
+++ frontend/src/app/objects/LineItemObject.tsx
@@ -1,4 +1,5 @@
 export interface LineItem {
+  item_id: number;
   title: string;
   amount: number;
 }
